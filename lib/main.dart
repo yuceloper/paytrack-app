da9d2655a180e7aa2d85ede4aaf8f8d62fb01764
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-25 23:00:44 +0300

    fix: initialize Turkish date formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'app/paytrack_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('tr_TR');
   runApp(const ProviderScope(child: PayTrackApp()));
 }
