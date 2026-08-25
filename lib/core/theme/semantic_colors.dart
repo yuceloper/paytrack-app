b4f8e8d4f6b48f477adc4f0f731ff6e2a4b4e8c2
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-26 00:44:28 +0300

    feat: add semantic finance colors

---
+++ lib/core/theme/semantic_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SemanticColors {
+  SemanticColors._();
+
+  static const Color income = Color(0xFF3D7A63);
+  static const Color incomeSoft = Color(0xFFEAF4EF);
+
+  static const Color expense = Color(0xFFA45757);
+  static const Color expenseSoft = Color(0xFFF7ECEC);
+
+  static const Color transfer = Color(0xFF59647A);
+  static const Color transferSoft = Color(0xFFEEF0F4);
+}
