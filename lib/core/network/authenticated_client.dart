a99474220e9c2d203e6fc4f57d91835f5ef0a1d5
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-26 02:49:12 +0300

    feat: refresh and retry expired API requests

--- lib/core/network/authenticated_client.dart
+++ lib/core/network/authenticated_client.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 
+import '../auth/auth_session_service.dart';
 import '../auth/session_store.dart';
 
 class AuthenticatedClient extends http.BaseClient {
@@ -8,12 +9,42 @@ class AuthenticatedClient extends http.BaseClient {
   AuthenticatedClient({http.Client? inner}) : _inner = inner ?? http.Client();
 
   @override
-  Future<http.StreamedResponse> send(http.BaseRequest request) {
+  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+    _attachAccessToken(request);
+    final retryRequest = _cloneRequest(request);
+
+    final response = await _inner.send(request);
+    if (response.statusCode != 401 || retryRequest == null) {
+      return response;
+    }
+
+    await response.stream.drain<void>();
+    await AuthSessionService.refreshSession();
+
+    _attachAccessToken(retryRequest);
+    return _inner.send(retryRequest);
+  }
+
+  void _attachAccessToken(http.BaseRequest request) {
     final token = SessionStore.accessToken;
     if (token != null && token.isNotEmpty) {
       request.headers['Authorization'] = 'Bearer $token';
+    } else {
+      request.headers.remove('Authorization');
     }
-    return _inner.send(request);
+  }
+
+  http.BaseRequest? _cloneRequest(http.BaseRequest request) {
+    if (request is! http.Request) return null;
+
+    final clone = http.Request(request.method, request.url)
+      ..headers.addAll(request.headers)
+      ..bodyBytes = request.bodyBytes
+      ..encoding = request.encoding
+      ..followRedirects = request.followRedirects
+      ..maxRedirects = request.maxRedirects
+      ..persistentConnection = request.persistentConnection;
+    return clone;
   }
 
   @override
