894c2e9de346b614a784c0f79f3a7cefd1f511c8
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-25 22:58:44 +0300

    feat: initialize Riverpod root scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'app/paytrack_app.dart';
 
 void main() {
-  runApp(const PayTrackApp());
+  runApp(const ProviderScope(child: PayTrackApp()));
 }
