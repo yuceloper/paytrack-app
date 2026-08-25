f481ec98be2a87c3f902062e7e67f368f998910e
yuceloper <155106626+yuceloper@users.noreply.github.com>
2026-08-26 00:52:52 +0300

    fix: use adaptive translucent semantic surfaces

--- lib/core/theme/semantic_colors.dart
+++ lib/core/theme/semantic_colors.dart
@@ -5,12 +5,15 @@ class SemanticColors {
 
   static const Color income = Color(0xFF3D7A63);
   static const Color incomeDark = Color(0xFF72B49A);
+  static const Color incomeSoft = Color(0x183D7A63);
 
   static const Color expense = Color(0xFFA45757);
   static const Color expenseDark = Color(0xFFD17B7B);
+  static const Color expenseSoft = Color(0x18A45757);
 
   static const Color transfer = Color(0xFF59647A);
   static const Color transferDark = Color(0xFF9AA6BE);
+  static const Color transferSoft = Color(0x1859647A);
 
   static Color incomeFor(BuildContext context) =>
       Theme.of(context).brightness == Brightness.dark ? incomeDark : income;
@@ -21,18 +24,7 @@ class SemanticColors {
   static Color transferFor(BuildContext context) =>
       Theme.of(context).brightness == Brightness.dark ? transferDark : transfer;
 
-  static Color incomeSoftFor(BuildContext context) =>
-      Theme.of(context).brightness == Brightness.dark
-          ? const Color(0xFF1B2B25)
-          : const Color(0xFFEAF4EF);
-
-  static Color expenseSoftFor(BuildContext context) =>
-      Theme.of(context).brightness == Brightness.dark
-          ? const Color(0xFF302121)
-          : const Color(0xFFF7ECEC);
-
-  static Color transferSoftFor(BuildContext context) =>
-      Theme.of(context).brightness == Brightness.dark
-          ? const Color(0xFF242832)
-          : const Color(0xFFEEF0F4);
+  static Color incomeSoftFor(BuildContext context) => incomeSoft;
+  static Color expenseSoftFor(BuildContext context) => expenseSoft;
+  static Color transferSoftFor(BuildContext context) => transferSoft;
 }
